3c1d97a3f82c02c8eb79d96c75b3b9e02b544430
miraz09 <hasibmiraz09@gmail.com>
2020-05-31 03:25:32 +0600

    Latest

--- Piggame/app.js
+++ Piggame/app.js
@@ -19,7 +19,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         var dice = Math.floor(Math.random() * 6) + 1;
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        diceDOM.src = 'images/dice-' + dice + '.png';
     
         if(dice !== 1 ) {
             roundScore += dice;
